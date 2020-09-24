0b6fa6e4b6927acec00f9342cbf5dcd0a4aa87de
Mattias Lundberg <me@mlundberg.se>
2020-09-24 18:08:21 +0200

    Prepare for deployment

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-const express = require("express");
-const { generateShortUrl, saveToDb, getFromDb } = require("./persistence");
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (_, res) => {
-  res.status(200).json({});
-});
-
-app.param("shortUrl", (req, res, next, shortUrl) => {
-  getFromDb(shortUrl)
-    .then((longUrl) => {
-      req.longUrl = longUrl;
-      next();
-    })
-    .catch(() => {
-      res.sendStatus(404);
-    });
-});
-
-app.get("/:shortUrl", ({ longUrl }, res) => {
-  res.redirect(longUrl);
-});
-
-app.post("/", async ({ body }, res) => {
-  const shortUrl = generateShortUrl();
-  await saveToDb(shortUrl, body.longUrl);
-  res.status(200).json({
-    longUrl: body.longUrl,
-    shortUrl,
-  });
-});
+const app = require("./app");
 
 const server = app.listen(3000);
 
