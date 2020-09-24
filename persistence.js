721800c9b6bed65c1a9d584d34118fa9e371bf57
Mattias Lundberg <me@mlundberg.se>
2020-09-24 16:00:41 +0200

    Fix tests not exiting cleanly

--- persistence.js
+++ persistence.js
@@ -1,13 +1,14 @@
 const redis = require("redis");
-const client = redis.createClient();
 
 const generateShortUrl = () => {
   return [...Array(12)].map(() => Math.random().toString(36)[2]).join("");
 };
 
 const saveToDb = (shortUrl, longUrl) => {
+  const client = redis.createClient();
   return new Promise((resolve, reject) => {
     client.set(shortUrl, longUrl, (e, res) => {
+      client.quit();
       if (e) {
         reject(new Error(e));
       } else {
@@ -18,8 +19,10 @@ const saveToDb = (shortUrl, longUrl) => {
 };
 
 const getFromDb = (shortUrl) => {
+  const client = redis.createClient();
   return new Promise((resolve, reject) => {
     client.get(shortUrl, (e, res) => {
+      client.quit();
       if (e || !res) {
         reject(new Error(e));
       } else {
