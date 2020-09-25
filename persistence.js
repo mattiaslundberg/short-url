0fbe3f78b6d74b20ed6b9683aaee0fa1fed7bddd
Mattias Lundberg <me@mlundberg.se>
2020-09-25 08:33:45 +0200

    Set redis host

--- persistence.js
+++ persistence.js
@@ -1,11 +1,12 @@
 const redis = require("redis");
+const { REDIS_URL } = process.env;
 
 const generateShortUrl = () => {
   return [...Array(12)].map(() => Math.random().toString(36)[2]).join("");
 };
 
 const saveToDb = (shortUrl, longUrl) => {
-  const client = redis.createClient();
+  const client = redis.createClient({ host: REDIS_URL });
   return new Promise((resolve, reject) => {
     client.set(shortUrl, longUrl, (e, res) => {
       client.quit();
@@ -19,7 +20,7 @@ const saveToDb = (shortUrl, longUrl) => {
 };
 
 const getFromDb = (shortUrl) => {
-  const client = redis.createClient();
+  const client = redis.createClient({ host: REDIS_URL });
   return new Promise((resolve, reject) => {
     client.get(shortUrl, (e, res) => {
       client.quit();
