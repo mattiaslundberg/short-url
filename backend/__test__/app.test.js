dbbc51f4b7ed6c62eb28cb2abaf8c8e0909ff909
Mattias Lundberg <me@mlundberg.se>
2020-09-27 10:20:15 +0200

    Remove test
    It doesn't work after serving static files

--- backend/__test__/app.test.js
+++ backend/__test__/app.test.js
@@ -1,23 +1,6 @@
 const request = require("supertest");
 const { getFromDb, saveToDb } = require("../persistence");
 
-describe("GET /", () => {
-  let server;
-  it("returns successful response", async () => {
-    const res = await request(server).get("/").send();
-
-    expect(res.statusCode).toBe(200);
-  });
-
-  beforeEach(() => {
-    server = require("../index");
-  });
-
-  afterEach(() => {
-    server.close();
-  });
-});
-
 describe("POST /", () => {
   let server;
 
